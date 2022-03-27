e5aaad9d0a3b753b0f0c248d94891e4cb3a50009
hsd9681 <hsd9681@naver.com>
2022-03-27 15:28:04 +0900

    롬복적용

--- src/main/java/com/hsd/exam1/User.java
+++ src/main/java/com/hsd/exam1/User.java
@@ -1,6 +1,8 @@
 package com.hsd.exam1;
 
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,13 +10,54 @@ import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public long id;
-    public LocalDateTime regDate;
-    public LocalDateTime updateDate;
-    public String email;
-    public String password;
-    public String name;
+    private long id;
+    private LocalDateTime regDate;
+    private LocalDateTime updateDate;
+    private String email;
+    private String password;
+    private String name;
+
+    public LocalDateTime getUpdateDate() {
+        return updateDate;
+    }
+
+    public void setUpdateDate(LocalDateTime updateDate) {
+        this.updateDate = updateDate;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }
